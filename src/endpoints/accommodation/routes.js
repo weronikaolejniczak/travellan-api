63cfb2105b93ffe2b84b642cbec6a6b0343522ac
weronikaolejniczak <wer.olejniczak@gmail.com>
2021-01-06 17:37:20 +0100

    (439) configure accommodation routes

---
+++ src/endpoints/accommodation/routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cheerio = require('cheerio');
+const request = require('request-promise');
+const url = require('url');
+
+const scrapeBooking = require('../../services/scrapeBooking');
+
+const Router = express.Router;
+const routes = new Router();
+
+// localhost:4001/v1/accommodation?region={region}&name={name}
+    // e.g.
+    // localhost:4001/v1/accommodation?region=ch&name=longemalle
+const getHotel = (req, res) => {    
+    const query = url.parse(req.url, true).query;
+    const region = query.region;
+    const name = query.name;
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    scrapeBooking(`https://www.booking.com/hotel/${region}/${name}.html`)
+        .then(function (result) { 
+            if (result === -1) console.log('Something went wrong...');
+            else res.json(result);
+    });
+};
+
+// GET /v1/accommodation
+routes.get('/', getHotel);
+
+module.exports = routes;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ let { requestListener } = require('./callback.js');
 
 const express = require('express');
 const constants = require('./config/constants');
+const apiEndpoints = require('./endpoints');
 
 const app = express();
+apiEndpoints(app);
+
 const PORT = constants.PORT;
 
-app.listen(PORT, requestListener);
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log('Error!');
+        throw err;
+    } else {
+        console.log(`Server is running at http://localhost:${PORT}/`);
+    }
+});
