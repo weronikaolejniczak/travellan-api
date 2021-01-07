26e59ea92735b98a8b5a52fde81385576b92805a
weronikaolejniczak <wer.olejniczak@gmail.com>
2021-01-07 20:48:18 +0100

    TRIPP-438 configure Amadeus

--- src/endpoints/accommodation/routes.js
+++ src/endpoints/accommodation/routes.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const url = require('url');
+const Amadeus = require('amadeus');
 
 const scrapeBooking = require('../../services/scrapeBooking');
 
 const Router = express.Router;
 const routes = new Router();
+const amadeus = new Amadeus({
+    clientId: process.env.AMADEUS_API_KEY,
+    clientSecret: process.env.AMADEUS_API_SECRET
+});
+
+/** 
+ * GET /v1/accommodation/recommendation?
+ * e.g.
+ * GET /v1/accommodation/recommendation?
+ * */
+
+const getHotelRecommendation = (req, res) => {
+    /* const query = url.parse(req.url, true).query;
+    const region = query.region;
+    const name = query.name; */
+
+    amadeus.shopping.flightOffersSearch
+        .get({
+            originLocationCode: 'SYD',
+            destinationLocationCode: 'BKK',
+            departureDate: '2021-04-01',
+            adults: '2'
+        })
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((err) => {
+            throw new Error(`Error ${err.code}: ${err.message}`);
+        });
+}
+
+routes.get('/recommendation', getHotelRecommendation);
+
+/** 
+ * GET /v1/accommodation/booking?region={region}&name={name}
+ * e.g.
+ * GET /v1/accommodation/booking?region=ch&name=longemalle
+ * */
 
 const getBookingHotel = (req, res) => {    
     const query = url.parse(req.url, true).query;
@@ -12,15 +51,12 @@ const getBookingHotel = (req, res) => {
     const name = query.name;
 
     scrapeBooking(`https://www.booking.com/hotel/${region}/${name}.html`)
-        .then(function (result) { 
-            if (result === -1) console.log('Something went wrong...');
+        .then((result) => { 
+            if (result === -1) throw new Error('Something went wrong...');
             else res.json(result);
     });
 };
 
-// GET /v1/accommodation/booking?region={region}&name={name}
-    // e.g.
-    // GET /v1/accommodation/booking?region=ch&name=longemalle
 routes.get('/booking', getBookingHotel);
 
 module.exports = routes;
